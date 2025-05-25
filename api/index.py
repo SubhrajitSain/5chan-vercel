45c8982a3bcf7509dda776dac3bc067efdfd3d5f
Subhrajit Sain <subhrajitsain34@gmail.com>
2025-05-25 10:36:24 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import uuid
